ffc77b6852459a5d65c8f43d695934ce5762e440
Nicola Liechti <nicola.liechti@students.fhnw.ch>
2024-03-07 13:16:46 +0100

    - Sections can now be changed over buttons - Added Header Content - Added transition for chang of sections - Added some styling for it (1.5 hour)

--- .idea/src/portfoliowebsite/script.js
+++ .idea/src/portfoliowebsite/script.js
@@ -1,15 +1,29 @@
 const sections = document.querySelectorAll('.section');
-const sectBtns = document.querySelectorAll('.controls');
-const sectBtn= document.querySelectorAll('.control');
+const sectBtns = document.querySelectorAll('.control');
 const allSections = document.querySelector('.main-content');
 
 function PageChange() {
-    for (let i = 0; i < sectBtn.length; i++) {
-        sectBtn[i].addEventListener('click', function(){
-            let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
-            this.className += ' active-btn';
-        })
-    }
+    sectBtns.forEach(btn => {
+        btn.addEventListener('click', function() {
+            const currentBtn = document.querySelector('.active-btn');
+            if (currentBtn) {
+                currentBtn.classList.remove('active-btn');
+            }
+            this.classList.add('active-btn');
+
+            const id = this.dataset.id;
+            if (id) {
+                sections.forEach((section) => {
+                    section.classList.remove('active');
+                });
+
+                const element = document.getElementById(id);
+                if (element) {
+                    element.classList.add('active');
+                }
+            }
+        });
+    });
 }
+
 PageChange();
